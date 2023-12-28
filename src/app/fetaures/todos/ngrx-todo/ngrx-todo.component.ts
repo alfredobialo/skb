a15a680c129bb7b39f5405de62543dc885cf463b
Alfred Obialo <alfredcsdinc@gmail.com>
2023-12-28 10:58:05 +0100

    Add new Todo Feature:dispatch() new Add Todo Event

--- src/app/fetaures/todos/ngrx-todo/ngrx-todo.component.ts
+++ src/app/fetaures/todos/ngrx-todo/ngrx-todo.component.ts
@@ -4,9 +4,10 @@ import {Observable} from 'rxjs';
 import {Store} from "@ngrx/store";
 import {AsyncPipe} from "@angular/common";
 import {addTodoAction, todoMarkedAction} from "../state/todo.actions";
-import {kebabCase} from "lodash";
+import kebabCase from "lodash/kebabCase";
+import identity from "lodash/identity";
 import {getTodoSelector} from "../state/todo.selectors";
-import {IAppState} from "../state/todo.state";
+import { ITodoState} from "../state/todo.state";
 
 @Component({
   selector: 'app-ngrx-todo',
@@ -20,9 +21,9 @@ import {IAppState} from "../state/todo.state";
 })
 export class NgrxTodoComponent {
 
-  todos$: Observable<TodoItemModel[]>;
+  todos$: Observable<ITodoState>;
 
-  constructor(private store: Store<{todos : IAppState}>) {
+  constructor(private store: Store<{todo : ITodoState}>) {
 
     this.todos$ = store.select(getTodoSelector);
   }
@@ -30,12 +31,20 @@ export class NgrxTodoComponent {
   markAsDone(t: TodoItemModel) {
     this.store.dispatch(todoMarkedAction({todoId: t.id, isDone: !t.isDone}));
     console.log(t);
+
+  }
+
+  createTodo(todoInput: HTMLInputElement) {
+    const todo = todoInput.value;
+
     this.store.dispatch(addTodoAction({
       todo: {
         isDone: false,
-        id: kebabCase("New Todo Added " + new Date()),
-        title: "New Todo Added " + new Date()
+        id: kebabCase(todo+identity()),
+        title: todo
       }
     }));
+
+    todoInput.value="";
   }
 }
