ee869670ca76f3c432e795100eb68e12485f054c
Alfred Obialo <alfredcsdinc@gmail.com>
2024-03-13 12:32:18 +0100

    Add Loading indication when app is INIT

--- src/app/shared/services/appInitHook.ts
+++ src/app/shared/services/appInitHook.ts
@@ -6,14 +6,20 @@ import {inject} from "@angular/core";
 export function appInitHook(appMenuService : AppMenuService) {
  const menuStore = inject(MenuStore);
   // access Route from Here
-  console.log("APP_INIT_CALLED", routes)
+  console.log("APP_INIT_CALLED", routes);
+
   const appMenus = () => appMenuService.getAppMenus()
     .pipe(tap({
       next : (x)=>{
         if (x.data) {
           console.log("SETTING MENU STORE VALUE => ", x.data);
           menuStore.setAppMenu(x.data);
+
         }
+      }, finalize : () => {
+        const elem = window.document.getElementById("app-loading");
+        console.log("app-loading element", elem);
+        elem?.remove();
       }
     }), map(x => {
       // map to new route here
